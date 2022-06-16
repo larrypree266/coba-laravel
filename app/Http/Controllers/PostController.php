8f6ba7332149056052911b03802af5e53bbde5ab
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-16 07:41:20 +0700

    01:fixing_category_pages

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            "title" => "Posts",
+            "title" => "All Posts",
             "posts" => Post::latest()->get(),
             // "posts" => Post::all(),
         ]);
