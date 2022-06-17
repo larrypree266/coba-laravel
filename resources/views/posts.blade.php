b7a0f5bd9ba03a9579d204802cacaa29d1da04a3
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-17 08:18:09 +0700

    03:02_fixing_searching_link_on_posts_for_category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -18,7 +18,6 @@
     {{-- count how many of the assigned data(variable) --}}
     @if ($posts->count())
         <div class="card mb-3 text-center">
-
             <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top"
                 alt="{{ $posts[0]->category->name }}">
             <div class="card-body">
@@ -28,7 +27,7 @@
                     <small class="text-muted">
                         By. <a href="/authors/{{ $posts[0]->author->username }}"
                             class="text-decoration-none">{{ $posts[0]->author->name }}</a> in
-                        <a href="/categories/{{ $posts[0]->category->slug }}"
+                        <a href="/posts?category/{{ $posts[0]->category->slug }}"
                             class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                         {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
@@ -48,7 +47,7 @@
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0,0,0,0.7)">
-                            <a href="/posts/{{ $posts[0]->slug }}"
+                            <a href="/posts?category=/{{ $post->category->slug }}"
                                 class="text-white text-decoration-none">{{ $post->category->name }}</a>
                         </div>
                         <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
