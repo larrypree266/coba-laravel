b4bfb0c1666c026981232090c962acdf484bfb4e
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-17 08:22:40 +0700

    03:04_fix_post_page_category_slug_url

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,7 +8,7 @@
 
                 <p>By. <a href="/authors/{{ $post->author->username }}"
                         class="text-decoration-none">{{ $post->author->name }}</a>
-                    in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                    in <a href="/posts?category={{ $post->category->slug }}">{{ $post->category->name }}</a>
                 </p>
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
                     alt="{{ $post->category->name }}" class="img-fluid">
