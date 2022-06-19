728053e16e6eb9709832b9a40b2a5c0fe0b1d6c7
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 15:17:41 +0700

    04:adding error when validation

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -8,21 +8,45 @@
                 <form action="/register" method="post">
                     @csrf
                     <div class="form-floating">
-                        <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="Name">
+                        <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror"
+                            id="name" placeholder="Name" required >
                         <label for="name">Name</label>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"
+                            id="username" placeholder="Username" required>
                         <label for="username">Username</label>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                            id="email" placeholder="name@example.com" required>
                         <label for="email">Email Address</label>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" name="password" class="form-control rounded-bottom" id="floatingPassword"
-                            placeholder="password">
+                        <input type="password" name="password"
+                            class="form-control rounded-bottom @error('email') is-invalid @enderror" id="floatingPassword"
+                            placeholder="Password" required>
                         <label for="password">Password</label>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
                     </forma>
