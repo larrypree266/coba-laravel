7247c88478ec5bc323d9ffa4223982a17e191568
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 15:03:59 +0700

    03:adding some logic into store function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,7 +15,16 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        return $request->all();
+        $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email|unique:users',
+            'password' =>  'required|min:5|max:255'
+        ]);
+
+        dd('registrasi berhasil!!');
+        // dd($request->all()); 
+        // return $request->all();
         // return request()->all();
     }
 }
