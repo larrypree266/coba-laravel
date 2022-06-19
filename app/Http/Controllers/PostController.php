ea9cab61921e22b5ea4b700d39dcd0d28601aec9
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 08:24:38 +0700

    05:05_fixing pagination to appear with query string from url

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
         return view('posts', [
             "title" => "All Posts" . $title,
             "active" => 'posts',
-            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7),
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString(),
         ]);
     }
 
