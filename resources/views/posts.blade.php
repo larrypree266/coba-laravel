23254a005f3486d0d053dca2922917df6e9b4e6a
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 08:19:06 +0700

    05:02_adding links on posts page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -43,36 +43,39 @@
 
             </div>
         </div>
+
+
+        <div class="container">
+            <div class="row">
+                @foreach ($posts->skip(1) as $post)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0,0,0,0.7)">
+                                <a href="/posts?category=/{{ $post->category->slug }}"
+                                    class="text-white text-decoration-none">{{ $post->category->name }}</a>
+                            </div>
+                            <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}"
+                                class="card-img-top" alt="{{ $post->category->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                                <p>
+                                    <small class="text-muted">
+                                        By. <a href="/posts?={{ $post->author->username }}"
+                                            class="text-decoration-none">{{ $post->author->name }}</a>
+                                        {{ $posts[0]->created_at->diffForHumans() }}
+                                    </small>
+                                </p>
+                                <p class="card-text">{{ $post->excerpt }}</p>
+                                <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                            </div>
+                        </div>
+                    </div> 
+                @endforeach
+            </div>
+        </div>
     @else
         <p class="text-center fs-4">No post found.</p>
     @endif
 
-    <div class="container">
-        <div class="row">
-            @foreach ($posts->skip(1) as $post)
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0,0,0,0.7)">
-                            <a href="/posts?category=/{{ $post->category->slug }}"
-                                class="text-white text-decoration-none">{{ $post->category->name }}</a>
-                        </div>
-                        <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
-                            alt="{{ $post->category->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $post->title }}</h5>
-                            <p>
-                                <small class="text-muted">
-                                    By. <a href="/posts?={{ $post->author->username }}"
-                                        class="text-decoration-none">{{ $post->author->name }}</a>
-                                    {{ $posts[0]->created_at->diffForHumans() }}
-                                </small>
-                            </p>
-                            <p class="card-text">{{ $post->excerpt }}</p>
-                            <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
+    {{ $posts->links() }}
 @endsection
