abf7b0aa1d16689219cbe354891f767a056a33a0
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 20:45:48 +0700

    08_adding flash messag

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -30,8 +30,8 @@ class RegisterController extends Controller
         $validatedData['password'] = Hash::make($validatedData['password']);
 
         User::create($validatedData);
-
-        return redirect('/login');
+        // $request->session()->flash('success', 'Registration successfull! Please login');
+        return redirect('/login')->with('success', 'Registration successfull! Please login');
 
         // dd('registrasi berhasil!!');
         // dd($request->all()); 
