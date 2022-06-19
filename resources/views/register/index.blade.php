b760fc6d8df202ed23be9cab2053543bec0c826c
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-19 14:50:36 +0700

    03:finalization

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -2,21 +2,30 @@
 
 @section('container')
     <div class="row justify-content-center">
-        <div class="col-md-5">
-            <main class="form-signin w-100 m-auto">
+        <div class="col-lg-5">
+            <main class="form-registration">
                 <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
                 <form>
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="Name">
+                        <label for="name">Name</label>
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
+                        <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                        <label for="username">Username</label>
                     </div>
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                    <div class="form-floating">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                        <label for="email">Email Address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control rounded-bottom" id="floatingPassword"
+                            placeholder="password">
+                        <label for="password">Password</label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
                 </form>
-                <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
+                <small class="d-block text-center mt-3">Already registered? <a href="/login">Login</a></small>
             </main>
         </div>
     </div>
