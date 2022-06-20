59a7c1200d4f4ec74e2a86d944453c6a4908cfd0
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-20 08:56:52 +0700

    08_showing data for designated user

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">My Posts</h1>
     </div>
-    <div class="table-responsive">
+    <div class="table-responsive col-lg-8">
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
@@ -15,13 +15,21 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                </tr>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->category->name }}</td>
+                        <td>
+                            <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-info"><span data-feather="eye"
+                                    class="align-text-bottom"></span></a>
+                            <a href="" class="badge bg-warning"><span data-feather="edit"
+                                    class="align-text-bottom"></span></a>
+                            <a href="" class="badge bg-danger"><span data-feather="x-circle"
+                                    class="align-text-bottom"></span></a>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
