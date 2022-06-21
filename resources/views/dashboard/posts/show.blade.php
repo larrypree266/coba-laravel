f2cd26d88be4618d357c028b46245ccfb9c66071
gordonmilkman373 <46396604+gordonmilkman373@users.noreply.github.com>
2022-06-21 09:37:50 +0700

    Delete:03_adding on show post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -10,15 +10,19 @@
                         class="align-text-bottom"></span> Back to
                     all my posts</a>
                 <a href="" class="btn btn-warning "><span data-feather="edit" class="align-text-bottom"></span> Edit</a>
-                <a href="" class="btn btn-danger "><span data-feather="x-circle" class="align-text-bottom"></span> Delete </a>
-
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger border-0" onclick="return confirm('Are you sure?')"><span
+                            data-feather="x-circle" class="align-text-bottom"></span>Delete</button>
+                </form>
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
                     alt="{{ $post->category->name }}" class="img-fluid mt-3">
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
                 </article>
 
-                
+
             </div>
         </div>
     </div>
